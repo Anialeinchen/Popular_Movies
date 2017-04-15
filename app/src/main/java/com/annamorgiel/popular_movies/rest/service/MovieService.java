f4b3c5e9381eb85f00dd78af6d9537eb3d184130
Anna Morgiel <morgiel@uni-potsdam.de>
2017-04-15 18:22:46 +0200

    Class detailActivity = DetailActivity.class;? in MainActivity

--- app/src/main/java/com/annamorgiel/popular_movies/rest/service/MovieService.java
+++ app/src/main/java/com/annamorgiel/popular_movies/rest/service/MovieService.java
@@ -5,6 +5,7 @@ import com.annamorgiel.popular_movies.rest.model.ApiResponse;
 import retrofit2.Callback;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 /**
  * Created by Anna Morgiel on 14.04.2017.
@@ -13,8 +14,8 @@ public interface MovieService {
 
     //public void getPopularMovies(@Path("id") Integer id, @Query("q") String name, Callback<ApiResponse> callback);
     //request data from the endpoints
-    @GET("/{id}/movie/popular")
-    public void getPopularMovies(@Path("id") Integer id, Callback<ApiResponse> callback);
+    @GET("/{id}/movie/{sortby}")
+    public void getPopularMovies(@Path("id") Integer id, @Query("sortby") String sortby, Callback<ApiResponse> callback);
 /*
     @GET("/{id}/movie/top_rated")
     public void getPopularMovies(@Path("id") Integer id, @Query("q") String name, Callback<ApiResponse> callback);
--- app/src/main/java/com/annamorgiel/popular_movies/ui/activity/DetailActivity.java
+++ app/src/main/java/com/annamorgiel/popular_movies/ui/activity/DetailActivity.java
@@ -1,24 +1,9 @@
 package com.annamorgiel.popular_movies.ui.activity;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.widget.ImageView;
-
-import com.annamorgiel.popular_movies.R;
-import com.annamorgiel.popular_movies.app.App;
-import com.annamorgiel.popular_movies.rest.model.ApiResponse;
-import com.squareup.picasso.Picasso;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-
 /**
  * Created by Anna Morgiel on 13.04.2017.
  */
-
+/*
 public class DetailActivity extends AppCompatActivity {
     Integer clickedItemId = null;
     String BASE_POSTER_URL = "http://image.tmdb.org/t/p/w185//";
@@ -53,3 +38,4 @@ public class DetailActivity extends AppCompatActivity {
         }
     }
 }
+*/
